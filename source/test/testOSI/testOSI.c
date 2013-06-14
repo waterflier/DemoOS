074bf9a7245ece5c34c43dc9951cd68399987a66
waterflier <waterflier@gmail.com>
2013-06-14 16:00:26 +0800

    commit

--- source/test/testOSI/testOSI.c
+++ source/test/testOSI/testOSI.c
@@ -1,7 +1,15 @@
 #include <pthread.h>
+#ifdef ANDROID
+#include <linux/sem.h>
+#include <linux/types.h>
+#include <linux/ipc.h>
+#include <linux/types.h>
+#else
 #include <sys/sem.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
+#include <sys/types.h>
+#endif
 #include <stdio.h>
 #include <unistd.h>
 #include <errno.h>
