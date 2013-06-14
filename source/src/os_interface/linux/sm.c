e18150c61ea3bd37fb0aeeeb8987a906d97e7366
Si Changjun <puotosssa@live.com>
2013-06-14 13:59:31 +0800

    先屏蔽shm调用

--- source/src/os_interface/linux/sm.c
+++ source/src/os_interface/linux/sm.c
@@ -9,10 +9,12 @@
 #include "../os_interface.h"
 #include "../utility.h"
 
+///android不支持sm这些  ， 先去掉
 
 int OSI_CreateSM(const char* smID,uint32_t size,TYPE_NGOS_SHAREMEMORY* pResult)
 {
 	//todo:bug 同id的无法互斥创建，下一次创建依旧能成功
+#ifndef ANDROID
 	int intid = GetStringKey(smID);
 	printf("sm create:%s = %X\n",smID,intid);
 	int smResult = shmget(intid,size,0640|IPC_CREAT|IPC_EXCL);
@@ -22,11 +24,13 @@ int OSI_CreateSM(const char* smID,uint32_t size,TYPE_NGOS_SHAREMEMORY* pResult)
 	}
 
 	*pResult = smResult;
+#endif
 	return 0;
 }
 
 int OSI_OpenSM(const char* smID,TYPE_NGOS_SHAREMEMORY* pResult)
 {
+#ifndef ANDROID
 	int intid = GetStringKey(smID);
 	printf("sm open:%s = %X\n",smID,intid);
 	int smResult = shmget(intid,0,0640);
@@ -36,16 +40,21 @@ int OSI_OpenSM(const char* smID,TYPE_NGOS_SHAREMEMORY* pResult)
 	}
 
 	*pResult = smResult;
+#endif
 	return 0;
 }
 
 int OSI_ReleaseSM(TYPE_NGOS_SHAREMEMORY hSM)
 {
+#ifndef ANDROID
 	return shmctl(hSM,IPC_RMID,NULL);
+#endif
+	return 0;
 }
 
 int OSI_LockSMBuffer(TYPE_NGOS_SHAREMEMORY hSM,unsigned char** ppResult)
 {
+#ifndef ANDROID
 	void* pResult = shmat(hSM,0,100);
 	if((int)pResult != -1)
 	{
@@ -54,9 +63,14 @@ int OSI_LockSMBuffer(TYPE_NGOS_SHAREMEMORY hSM,unsigned char** ppResult)
 	}
 
 	return -1;
+#endif
+	return 0;
 }
 int OSI_UnlockSMBuffer(TYPE_NGOS_SHAREMEMORY hSM,unsigned char* pBuffer)
 {
+#ifndef ANDROID
 	int result = shmdt(pBuffer);
 	return result;
+#endif
+	return 0;
 }
